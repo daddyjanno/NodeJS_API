6e924bc3cbc2320d1ca8b4d1b2beb68742943147
daddyjanno <jn.drugmand@gmail.com>
2024-09-03 14:48:09 +0200

    create route for photographers list and for photographer by id

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+
+//our sample database
 const db = require('./data/photographers.json')
 
 const port = process.env.PORT || 5000
@@ -18,8 +20,31 @@ app.options('*', cors())
 
 //configure routes
 const router = express.Router()
-router.get('/', (request, response) => {
-    response.send(`${db.name}`)
+router.get('/', (req, res) => {
+    res.send(`${db.name}`)
+})
+
+router.get('/photographers/', (req, res) => {
+    const photographersList = db.photographers
+
+    if (!photographersList) {
+        return res.status(404).json({ error: 'No data for photographers' })
+    }
+
+    return res.json(photographersList)
+})
+
+router.get('/photographers/:id', (req, res) => {
+    const user = req.params.id
+    const photographer = db.photographers.find((el) => el.id === Number(user))
+
+    if (!photographer) {
+        return res
+            .status(404)
+            .json({ error: 'Photographer does not exist', photographer })
+    }
+
+    return res.json(photographer)
 })
 
 //register routes
