e8aa7914b5b3349496fa442df4480b4c5407d57f
daddyjanno <jn.drugmand@gmail.com>
2024-09-06 13:01:27 +0200

    server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const sqlite3 = require('sqlite3').verbose()
 
 // open the database
-let db = new sqlite3.Database('./db/FishEye.db', sqlite3.OPEN_CREATE, (err) => {
-    if (err) {
-        console.error(err.message)
+let db = new sqlite3.Database(
+    './db/FishEye.db',
+    sqlite3.OPEN_READWRITE,
+    (err) => {
+        if (err) {
+            console.error(err.message)
+        }
+        console.log('Connected to the FishEye database.')
     }
-    console.log('Connected to the FishEye database.')
-})
+)
 
 // db.serialize(() => {
 //     db.each(
