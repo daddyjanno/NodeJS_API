0c841ee951a224639830514269d0ed157ae2ce46
daddyjanno <jn.drugmand@gmail.com>
2024-09-10 15:24:19 +0200

    delete unused files

--- server/routes/photographerRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const photographerController = require('../controller/photographerController')
-
-router.post('/signup', photographerController.createPhotographer)
-
-module.exports = router
